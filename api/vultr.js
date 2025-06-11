e176820f9cb0a0d7bc7593ac57c64022f5dfdaf4
leadingservers <john@leadingservers.com>
2025-06-11 06:33:27 -0400

    Fix CommonJS export for Vercel API

--- api/vultr.js
+++ api/vultr.js
@@ -1,5 +1,5 @@
 
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   const servers = [
     { name: "Vultr", cpu: 4, ram: 16, storage: 500, price: 80, location: "North America", link: "https://www.vultr.com/?ref=affiliate123" }
   ];
